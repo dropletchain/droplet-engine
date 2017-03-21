7286f3cf2958bbcfa58b74141978b196a40d1d9d
Lukas Burkhalter <lukas.burkhalter@ringwil.ch>
2017-03-21 14:07:01 +0100

    setup module

---
+++ talosblockchain/talosvc/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='talos-virtualchain',
+      version='0.1',
+      description='TalosVirtualchain',
+      url='https://github.com/hosseinsh/blockchain/tree/master/talosblockchain/talosvc',
+      author='Talos',
+      author_email='lubu@student.ethz.ch',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
