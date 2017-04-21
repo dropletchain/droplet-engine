790aea74e73037266e264e871af43b4af0e490fa
Lukas Burkhalter <lukas.burkhalter@ringwil.ch>
2017-04-21 15:05:29 +0200

    more docs

--- talosblockchain/talosstorage/checks.py
+++ talosblockchain/talosstorage/checks.py
@@ -37,9 +37,9 @@ class BitcoinVersionedPrivateKey(BitcoinPrivateKey):
 
 def get_crypto_ecdsa_pubkey_from_bitcoin_hex(bitcoin_hex_key):
     """
-    Given a bitcoin hex key returns the corresponding cryptography ECDSA key object
+    Given a bitcoin hex string key returns the corresponding cryptography ECDSA key object
     :param bitcoin_hex_key: the h
-    :return: 
+    :return: cryptography ECDSA key object
     """
     bin_ecdsa_public_key = extract_bin_ecdsa_pubkey(bitcoin_hex_key)
     numbers = EllipticCurvePublicNumbers.from_encoded_point(ec.SECP256K1(), b'\x04' + bin_ecdsa_public_key)
@@ -47,31 +47,68 @@ def get_crypto_ecdsa_pubkey_from_bitcoin_hex(bitcoin_hex_key):
 
 
 def get_bitcoin_address_for_pubkey(hex_pubkey):
+    """
+    Computes the bitcoin address given a bitcoin public key in hex format 
+    :param hex_pubkey: the bitcoin public key in hex format
+    :return: bitcoin address hex string
+    """
     priv = extract_bin_bitcoin_pubkey(hex_pubkey)
     hash_priv = get_bin_hash160(priv)
     return bin_hash160_to_address(hash_priv, version_byte=USED_VERSIONBYTE)
 
 
 def get_stream_identifier_from_policy(policy):
+    """
+    Given a policy, extracts a StreamIdentifier object
+    :param policy: a Talos policy object
+    :return: a StreamIdentifier object
+    """
     return DataStreamIdentifier(policy.owner, policy.stream_id, policy.get_nonce_bin(), policy.txid)
 
 
 def check_key_matches(cloud_chunk, policy, chunkid):
+    """
+    Ficen a cloud chunk object and a policy with the chunkdi checks if the key matches
+    :param cloud_chunk: a cloud chunk object
+    :param policy: a policy object
+    :param chunkid: (int) chunk identifier 
+    :return: True if ok else False
+    """
     stream_ident = get_stream_identifier_from_policy(policy)
     return cloud_chunk.key == stream_ident.get_key_for_blockid(chunkid)
 
 
 def check_tag_matches(cloud_chunk, policy):
+    """
+    Given a cloud chunk and a policy, checks if the chunk is linke to the policy with the 
+    tag
+    :param cloud_chunk: a cloud chunk object
+    :param policy: a policy object
+    :return: True if ok else False
+    """
     stream_ident = get_stream_identifier_from_policy(policy)
     return cloud_chunk.policy_tag == stream_ident.get_tag()
 
 
 def check_signature(cloud_chunk, policy):
+    """
+    Given a cloud chunk and a policy, checks if the cloud chunk has a valid signature 
+    :param cloud_chunk: a cloud chunk object
+    :param policy: a policy object
+    :return: True if ok else False
+    """
     pub_key = get_crypto_ecdsa_pubkey_from_bitcoin_hex(str(policy.owner_pk))
     return cloud_chunk.check_signature(pub_key)
 
 
 def check_access_allowed(hex_pubkey, policy):
+    """
+    Given a bicoin hex string public key and a policy, checks if the public key is 
+    the owner or in the shares list
+    :param hex_pubkey: the bitcoin hex string key
+    :param policy: a policy object
+    :return: True if owner or in shares else False
+    """
     addr = get_bitcoin_address_for_pubkey(str(hex_pubkey))
     if addr == policy.owner:
         return True
@@ -81,11 +118,25 @@ def check_access_allowed(hex_pubkey, policy):
 
 
 def check_access_key_valid(access_key, policy, blockid):
+    """
+    Chekcs if the access key matches the policy and the chunk identifier.
+    :param access_key: hex key
+    :param policy: the policy
+    :param blockid: the id of the chunk
+    :return: Ture if ok else False
+    """
     stream_ident = get_stream_identifier_from_policy(policy)
     return access_key == stream_ident.get_key_for_blockid(blockid)
 
 
 def check_pubkey_valid(data, signature, pubkey):
+    """
+    Checks if the data has a valid signature with the given public key.
+    :param data: the data
+    :param signature: the signature
+    :param pubkey: the bitcoin public key as a hex string
+    :return: 
+    """
     pub_key = get_crypto_ecdsa_pubkey_from_bitcoin_hex(pubkey)
     try:
         check_signed_data(pub_key, signature, data)
@@ -103,7 +154,21 @@ JSON_PUB_KEY = "pubkey"
 
 
 class QueryToken(object):
+    """
+    Represents a token for retrieving a specific chunk. 
+    Contains an owner, the stream id, a nonce, the chunk retrival key, a signature and 
+    the the public key.
+    """
     def __init__(self, owner, streamid, nonce, chunk_key, signature, pubkey):
+        """
+        Inits a query token
+        :param owner: the bitcoin address of the stream owner
+        :param streamid: the stream identifier (int)
+        :param nonce: a challenge nonce against replays 
+        :param chunk_key: the chunk key for retreiving the chunk from the storage
+        :param signature: a signature of the token
+        :param pubkey: the public key hey string of the signature
+        """
         self.owner = owner
         self.streamid = streamid
         self.nonce = nonce
@@ -139,6 +204,11 @@ class QueryToken(object):
 
 
 def check_valid(token):
+    """
+    Given a query token checks if the token is valid. (Signature check)
+    :param token: the QueryToken object
+    :return: True if ok else False
+    """
     data = token.get_signature_data()
     if not check_pubkey_valid(data, token.signature, token.pubkey):
         return False
@@ -148,6 +218,11 @@ def check_valid(token):
 
 
 def check_json_query_token_valid(json_msg):
+    """
+    Checks if a json msg contains all query token json fields
+    :param json_msg: the json message as map
+    :return: True if ok else False
+    """
     return JSON_OWNER in json_msg \
            and JSON_STREAM_ID in json_msg \
            and JSON_NONCE in json_msg \
@@ -157,11 +232,26 @@ def check_json_query_token_valid(json_msg):
 
 
 def get_priv_key(bvpk_private_key):
+    """
+    Given a pybitcoin private key computes the corresponding cryptography private key object.
+    :param bvpk_private_key: a pybitcoin private key
+    :return: cryptography ECDSA private key object
+    """
     private_value = long(bvpk_private_key.to_hex()[:-2], 16)
     return ec.derive_private_key(private_value, ec.SECP256K1(), default_backend())
 
 
 def generate_query_token(owner, streamid, nonce, chunk_key, bvpk_private_key):
+    """
+    Generates a QueryToken object given the owner, stream_id, nonce, chunk retrival key and 
+    a pybitcoin private key object
+    :param owner: the bitcoin addres of the stream ownder (hex string)
+    :param streamid: the stream id (int)
+    :param nonce: the challange nonce
+    :param chunk_key: the chunk retrival key
+    :param bvpk_private_key: a BitcoinVersionedPrivateKey object
+    :return: a QueryToken object
+    """
     private_key = get_priv_key(bvpk_private_key)
     data = owner + str(streamid) + str(nonce) + chunk_key
     signature = hash_sign_data(private_key, data)
@@ -177,6 +267,11 @@ class InvalidQueryToken(Exception):
 
 
 def get_and_check_query_token(json_token):
+    """
+    Given a json query token as map, returns the QueryToken object.
+    :param json_token: the json msg as map
+    :return: a QueryToken objec if ok t, else throws InvalidQueryToken exception
+    """
     if json_token is None:
         raise InvalidQueryToken("ERROR No signature supplied")
     if not check_json_query_token_valid(json_token):
@@ -185,6 +280,11 @@ def get_and_check_query_token(json_token):
 
 
 def check_query_token_valid(query_token):
+    """
+    Given a QueryToken object, checks if the token is valid
+    :param query_token: the QueryToken object
+    :return: True if ok else throws InvalidQueryToken exception
+    """
     if not check_valid(query_token):
         raise InvalidQueryToken("ERROR pulibc key not valid")
     return True
--- talosblockchain/talosstorage/storage.py
+++ talosblockchain/talosstorage/storage.py
@@ -5,6 +5,11 @@ from chunkdata import CloudChunk
 from talosstorage.util import *
 
 
+"""
+Implementation of the talos storage, which checks each access against a policy.
+"""
+
+
 class InvalidChunkError(Exception):
     def __init__(self, value):
         self.value = value
@@ -22,7 +27,19 @@ class InvalidAccess(Exception):
 
 
 class TalosStorage(object):
+    """
+    High level interface class for a Talos storage
+    """
+
     def check_chunk_valid(self, chunk, policy, chunk_id=None, time_keeper=TimeKeeper()):
+        """
+        Given a CloudChunk object and a Policy object, checks if the chunk is valid
+        :param chunk: a CloudChunk object
+        :param policy: a Policy object
+        :param chunk_id: the chunk_id (optional)
+        :param time_keeper: benchamrk object
+        :return: Nothing throws InvalidChunkError if chunk not valid
+        """
         time_keeper.start_clock()
         if not chunk_id is None:
             if not check_key_matches(chunk, policy, chunk_id):
@@ -42,10 +59,24 @@ class TalosStorage(object):
         time_keeper.stop_clock(ENTRY_PUT_CHECK_CHUNK_SIGNATURE)
 
     def check_access_valid(self, pubkey, policy):
+        """
+        Given a public key and a Policy object, checks if the access is valid
+        :param pubkey: the hex string public key
+        :param policy: the policy object
+        :return: Nothing throws InvalidChunkError if chunk not valid
+        """
         if not check_access_allowed(pubkey, policy):
             raise InvalidAccess("Pubkey not in policy")
 
     def store_check_chunk(self, chunk, chunk_id, policy, time_keeper=TimeKeeper()):
+        """
+        Store the given CloudChunk if it validates against the provided Policy object 
+        :param chunk: a CloudChunk object
+        :param chunk_id: the id of the chunk (Can be None if not known)
+        :param policy: the Policy object
+        :param time_keeper: benchamrk object
+        :return: True if ok else throws InvalidChunkError
+        """
         time_keeper.start_clock()
         self.check_chunk_valid(chunk, policy, chunk_id=chunk_id, time_keeper=time_keeper)
         time_keeper.stop_clock(ENTRY_PUT_CHECK_CHUNK)
@@ -56,6 +87,15 @@ class TalosStorage(object):
         return result_store
 
     def get_check_chunk(self, chunk_key, pubkey, policy, time_keeper=TimeKeeper()):
+        """
+        Given a chunk retrival key, a public key and a policy object, tries to
+        retrive the corresponding chunk
+        :param chunk_key: the chunk retrieval key
+        :param pubkey: the hex string public key
+        :param policy: the Policy object
+        :param time_keeper: benchmark object
+        :return: 
+        """
         time_keeper.start_clock()
         self.check_access_valid(pubkey, policy)
         time_keeper.stop_clock(ENTRY_GET_CHECK_ACCESS)
@@ -78,6 +118,9 @@ class TalosStorage(object):
 
 
 class LevelDBStorage(TalosStorage):
+    """
+    Simple level db implementation of the talos storage for testing
+    """
     def __init__(self, db_dir):
         self.db = leveldb.LevelDB(db_dir)
 
