ed6c8ba066bab220880628f8fc6eed803d43a6ea
Lukas Burkhalter <lukas.burkhalter@ringwil.ch>
2017-04-28 15:34:02 +0200

    bench par dht

---
+++ experiments/scripts/bench_dht_local_fetch.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+LOCAL_PATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+CUR_PATH=$(pwd)
+
+PROJECT_PATH="talosblockchain/"
+#NUM_NODES_LIST={16,32}
+KVALUE=10
+ALPHA=3
+SECURE=1
+ITERATIONS=100
+NUM_FETCH=1000
+NUM_THREADS=32
+
+BENCHMARK_NAME="local_dht_benchmark_par_fetch_k${KVALUE}_a${ALPHA}"
+
+cd $LOCAL_PATH
+cd ../../$PROJECT_PATH
+
+if hash python2.7 2>/dev/null; then
+    cmd=python2.7
+else
+    cmd=python
+fi
+
+echo "Assumes bitcoin + virtualchain node runs"
+#echo "Run virtualchain"
+#./scripts/run_rest_vc.sh
+
+
+sleep 2
+
+mkdir $BENCHMARK_NAME
+echo "Experiment starts"
+for LATENCY in {10,}
+do
+	if [ "$(uname)" == "Darwin" ]; then
+		echo "latency not supportet with netem"
+	else
+		if [[ ! -z  $LATENCY ]]; then
+			echo "Add latency ${LATENCY}ms"
+			sudo tc qdisc add dev lo root netem delay ${LATENCY}ms
+		fi
+	fi
+
+	SLEEP_TIME=10
+	for NUM_NODES in {16,32,64,128,256,512,1024}
+	do
+		echo "Run $NUM_NODES dht nodes"
+		NUM_HELPER_NODES=$((NUM_NODES-1))
+		./scripts/run_dht_nodes.sh 127.0.0.1 $NUM_HELPER_NODES $SECURE $KVALUE $ALPHA
+
+		echo "DHT nodes run wait bootstrap ${SLEEP_TIME}s..."
+		sleep $SLEEP_TIME
+		ROUND_NAME=${BENCHMARK_NAME}_n${NUM_NODES}_l${LATENCY}
+		echo "Start benchmark"
+		$cmd ./benchmark/benchmark_api.py --log_db "$BENCHMARK_NAME/$x.db" --num_rounds $ITERATIONS --do_fetch True --num_fetches $NUM_FETCH --num_threads $NUM_THREADS
+
+		echo "Extract data from logs"
+		$cmd ./benchmark/logextraction.py --logpath ./dhtstorage/logs --dbname "$BENCHMARK_NAME/$ROUND_NAME.db"
+
+		echo "Terminate Nodes"
+		./scripts/terminate_dht_nodes.sh
+
+		echo "Remove DHT Files"
+		rm -r ./dhtstorage
+
+		#SLEEP_TIME=$((SLEEP_TIME*2))
+	done
+
+	if [ "$(uname)" == "Darwin" ]; then
+		echo "latency not supportet with netem"
+	else
+		if [[ ! -z  $LATENCY ]]; then
+			echo "Remove latency ${LATENCY}ms"
+			sudo tc qdisc del dev lo root netem
+		fi  
+	fi
+done
+
+echo "Benchmark done move results"
+mv $BENCHMARK_NAME $LOCAL_PATH/../data
+
+cd $CUR_PATH
+
--- talosblockchain/benchmark/benchmark_api.py
+++ talosblockchain/benchmark/benchmark_api.py
@@ -3,6 +3,7 @@ import base64
 import os
 import random
 import sys
+import threading
 import time
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
@@ -64,6 +65,77 @@ def run_benchmark_store_get(num_rounds, out_logger, private_key=BitcoinVersioned
     print "DONE"
 
 
+class Fetchjob(threading.Thread):
+    def __init__(self, my_id, results, dht_api_client, roundns, private_key, identifier):
+        self.results = results
+        self.my_id = my_id
+        self.dht_api_client = dht_api_client
+        self.identifier = identifier
+        self.private_key = private_key
+        self.roundns = roundns
+        threading.Thread.__init__(self)
+
+    def run(self):
+        for block_id in self.roundns:
+            try:
+                chunk = self.dht_api_client.fetch_chunk(block_id, self.private_key, self.identifier)
+                self.results[self.my_id].append(chunk)
+            except Exception as e:
+                print e
+                self.results[self.my_id].append(None)
+                continue
+
+
+def splitting(l, n):
+    mod = len(l) % n
+    size = len(l) / n
+    offset = 0
+    for i in xrange(0, len(l) - mod, size):
+        if i / size < mod:
+            yield l[i + offset:i + offset + size + 1]
+            offset += 1
+        else:
+            yield l[i + offset:i + size + offset]
+
+
+def run_benchmark_fetch_par(num_rounds, num_fetches, num_threads, out_logger,
+                            private_key=BitcoinVersionedPrivateKey(PRIVATE_KEY),
+                            policy_nonce=base64.b64decode(NONCE), stream_id=STREAMID,
+                            txid=TXID, ip=IP, port=PORT, chunk_size=100000):
+    key = os.urandom(32)
+    identifier = DataStreamIdentifier(private_key.public_key().address(), stream_id, policy_nonce, txid)
+
+    dht_api_client = DHTRestClient(dhtip=ip, dhtport=port)
+
+    for round_bench in range(num_fetches):
+        try:
+            chunk = generate_random_chunk(private_key, round_bench, identifier, key=key, size=chunk_size)
+            dht_api_client.store_chunk(chunk)
+            print "Store chunk %d" % (round_bench,)
+        except DHTRestClientException as e:
+            print "Store round %d error: %s" % (round_bench, e)
+
+    for round in range(num_rounds):
+        time_keeper = TimeKeeper()
+        results = [[]] * num_threads
+        threads = [Fetchjob(idx, results, DHTRestClient(dhtip=ip, dhtport=port), block_id, private_key, identifier)
+                   for idx, block_id in enumerate(splitting(range(num_fetches), num_threads))]
+        time_keeper.start_clock()
+        map(lambda x: x.start(), threads)
+        map(lambda x: x.join(), threads)
+        time_keeper.stop_clock("time_fetch_all")
+        chunks = [item for sublist in results for item in sublist]
+        if len(chunks) == num_fetches:
+            print "Round %d ok Num results: %d" % (round, num_fetches)
+        else:
+            print "Round %d ok Num results: %d" % (round, num_fetches)
+        for idx, chunk in enumerate(chunks):
+            if chunk is None:
+                print "No result for chunk %d " % idx
+        out_logger.log_times_keeper(time_keeper)
+    print "DONE"
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Run benchmark")
     parser.add_argument('--dhtport', type=int, help='dhtport', default=PORT, required=False)
@@ -76,9 +148,15 @@ if __name__ == "__main__":
     parser.add_argument('--chunk_size', type=int, help='chunk_size', default=10000, required=False)
     parser.add_argument('--log_db', type=str, help='log_db', default=None, required=False)
     parser.add_argument('--name', type=str, help='name', default="CLIENT_STORE_GET", required=False)
-    args = parser.parse_args()
 
-    LOGGING_FIELDS = [TIME_STORE_CHUNK, TIME_FETCH_ADDRESS, TIME_FETCH_NONCE, TIME_FETCH_CHUNK]
+    parser.add_argument('--do_fetch', type=bool, help='do_fetch', default=False, required=False)
+    parser.add_argument('--num_fetches', type=int, help='chunk_size', default=100, required=False)
+    parser.add_argument('--num_threads', type=int, help='num_threads', default=16, required=False)
+    args = parser.parse_args()
+    if args.do_fetch:
+        LOGGING_FIELDS = ["time_fetch_all"]
+    else:
+        LOGGING_FIELDS = [TIME_STORE_CHUNK, TIME_FETCH_ADDRESS, TIME_FETCH_NONCE, TIME_FETCH_CHUNK]
 
     if args.log_db is None:
         logger = FileBenchmarkLogger("%s_%d.log" % (args.name, args.num_rounds), LOGGING_FIELDS)
@@ -88,8 +166,14 @@ if __name__ == "__main__":
     try:
         private_key = BitcoinVersionedPrivateKey(args.private_key)
         policy_nonce = base64.b64decode(args.nonce)
-        run_benchmark_store_get(args.num_rounds, logger, private_key=private_key, policy_nonce=policy_nonce,
-                                stream_id=args.stream_id, txid=args.txid,
-                                ip=args.dhtserver, port=args.dhtport, chunk_size=args.chunk_size)
+        if args.do_fetch:
+            run_benchmark_fetch_par(args.num_rounds, args.num_fetches, args.num_threads, logger,
+                                    private_key=private_key, policy_nonce=policy_nonce,
+                                    stream_id=args.stream_id, txid=args.txid,
+                                    ip=args.dhtserver, port=args.dhtport, chunk_size=args.chunk_size)
+        else:
+            run_benchmark_store_get(args.num_rounds, logger, private_key=private_key, policy_nonce=policy_nonce,
+                                    stream_id=args.stream_id, txid=args.txid,
+                                    ip=args.dhtserver, port=args.dhtport, chunk_size=args.chunk_size)
     finally:
         logger.close()
