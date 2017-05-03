fb773437c20bd15205d14b19b55c6e2fc52cb30d
Lukas Burkhalter <lukas.burkhalter@ringwil.ch>
2017-05-03 10:13:25 +0200

    paper benchmark fix

--- experiments/eval_scripts/paper_plot_compression.py
+++ experiments/eval_scripts/paper_plot_compression.py
@@ -8,87 +8,93 @@ import os
 import math
 import numpy as np
 
-FITBIT = "fb"
-AVA = "av"
-SMART_METER = "smam"
-
-types = [FITBIT, AVA, SMART_METER]
-
-files = {
-    FITBIT: "chunk_bench_Fitbit_5605640.csv",
-    AVA: "chunk_bench_AVA_525600.csv",
-    SMART_METER: "chunk_bench_SmartMeter.csv"
-}
 
 
-path = "../data/chunk_compression_data"
+def plot_compression():
+    FITBIT = "fb"
+    AVA = "av"
+    SMART_METER = "smam"
 
-result = {}
-for compdata in types:
-    file_path = os.path.join(path, files[compdata])
-    with open(file_path, 'r') as data_file:
-        temp_data = []
-        for idx, line in enumerate(data_file):
-            if idx > 0:
-                temp_data.append(map(float, line.split(",")))
-        result[compdata] = np.asarray(temp_data)
+    types = [FITBIT, AVA, SMART_METER]
 
-########
-# PLOT #
-########
-
-#---------------------------- GLOBAL VARIABLES --------------------------------#
-# figure settings
-fig_width_pt = 300.0                        # Get this from LaTeX using \showthe
-inches_per_pt = 1.0/72.27*2                 # Convert pt to inches
-golden_mean = ((math.sqrt(5)-1.0)/2.0)*.8   # Aesthetic ratio
-fig_width = fig_width_pt*inches_per_pt      # width in inches
-fig_height =(fig_width*golden_mean)           # height in inches
-fig_size = [fig_width,fig_height/1.22]
-
-params = {'backend': 'ps',
-    'axes.labelsize': 18,
-    'legend.fontsize': 16,
-    'xtick.labelsize': 16,
-    'ytick.labelsize': 16,
-    'font.size': 16,
-    'figure.figsize': fig_size,
-    'font.family': 'times new roman'}
+    files = {
+        FITBIT: "chunk_bench_Fitbit_5605640.csv",
+        AVA: "chunk_bench_AVA_525600.csv",
+        SMART_METER: "chunk_bench_SmartMeter.csv"
+    }
 
 
-pdf_pages = PdfPages('../plots/plot_compression-rate.pdf')
-fig_size = [fig_width, fig_height / 1.8]
+    path = "../data/chunk_compression_data"
 
-plt.rcParams.update(params)
-plt.axes([0.12, 0.32, 0.85, 0.63], frameon=True)
-plt.rc('pdf', fonttype=42)  # IMPORTANT to get rid of Type 3
+    result = {}
+    for compdata in types:
+        file_path = os.path.join(path, files[compdata])
+        with open(file_path, 'r') as data_file:
+            temp_data = []
+            for idx, line in enumerate(data_file):
+                if idx > 0:
+                    temp_data.append(map(float, line.split(",")))
+            result[compdata] = np.asarray(temp_data)
 
-colors = ['0.1', '0.3', '0.6']
-linestyles = ['-', '--', '-']
+    ########
+    # PLOT #
+    ########
 
-data_ava = result[AVA]
-plt.semilogx(data_ava[:, 0], data_ava[:, 2], '-o', label='Ava dataset', color=colors[0], linestyle=linestyles[0], linewidth=2)
+    #---------------------------- GLOBAL VARIABLES --------------------------------#
+    # figure settings
+    fig_width_pt = 300.0                        # Get this from LaTeX using \showthe
+    inches_per_pt = 1.0/72.27*2                 # Convert pt to inches
+    golden_mean = ((math.sqrt(5)-1.0)/2.0)*.8   # Aesthetic ratio
+    fig_width = fig_width_pt*inches_per_pt      # width in inches
+    fig_height =(fig_width*golden_mean)           # height in inches
+    fig_size = [fig_width,fig_height/1.22]
 
-data_smart = result[SMART_METER]
-plt.semilogx(data_smart[:, 0], data_smart[:, 1], '-o', label='SmartMeter dataset', color=colors[1], linestyle=linestyles[1],
-             linewidth=2)
+    params = {'backend': 'ps',
+        'axes.labelsize': 18,
+        'legend.fontsize': 16,
+        'xtick.labelsize': 16,
+        'ytick.labelsize': 16,
+        'font.size': 16,
+        'figure.figsize': fig_size,
+        'font.family': 'times new roman'}
 
-data_fitbit = result[FITBIT]
-plt.semilogx(data_fitbit[:, 0], data_fitbit[:, 2], '-o', label='FitBit dataset', color=colors[2], linestyle=linestyles[2],
-             linewidth=2)
 
-plt.semilogx(1, 1)
-plt.xlabel('Number of Chunk Entries')
-plt.xlim(xmin=2, xmax=300000)
+    pdf_pages = PdfPages('../plots/plot_compression-rate.pdf')
+    fig_size = [fig_width, fig_height / 1.8]
 
-plt.ylabel('Compression Ratio')
-plt.ylim(ymin=0, ymax=12)
+    plt.rcParams.update(params)
+    plt.axes([0.12, 0.32, 0.85, 0.63], frameon=True)
+    plt.rc('pdf', fonttype=42)  # IMPORTANT to get rid of Type 3
 
-plt.legend(bbox_to_anchor=(0., 1.08, 1., .102), loc=3, ncol=4)
+    colors = ['0.1', '0.3', '0.6']
+    linestyles = ['-', '--', '-']
 
-plt.grid(True, linestyle=':', color='0.8', zorder=0)
-F = plt.gcf()
-F.set_size_inches(fig_size)
-pdf_pages.savefig(F, bbox_inches='tight')
-plt.clf()
-pdf_pages.close()
+    data_ava = result[AVA]
+    plt.semilogx(data_ava[:, 0], data_ava[:, 2], '-o', label='Ava dataset', color=colors[0], linestyle=linestyles[0], linewidth=2)
+
+    data_smart = result[SMART_METER]
+    plt.semilogx(data_smart[:, 0], data_smart[:, 1], '-o', label='SmartMeter dataset', color=colors[1], linestyle=linestyles[1],
+                 linewidth=2)
+
+    data_fitbit = result[FITBIT]
+    plt.semilogx(data_fitbit[:, 0], data_fitbit[:, 2], '-o', label='FitBit dataset', color=colors[2], linestyle=linestyles[2],
+                 linewidth=2)
+
+    plt.semilogx(1, 1)
+    plt.xlabel('Number of Chunk Entries')
+    plt.xlim(xmin=2, xmax=300000)
+
+    plt.ylabel('Compression Ratio')
+    plt.ylim(ymin=0, ymax=12)
+
+    plt.legend(bbox_to_anchor=(0., 1.08, 1., .102), loc=3, ncol=4)
+
+    plt.grid(True, linestyle=':', color='0.8', zorder=0)
+    F = plt.gcf()
+    F.set_size_inches(fig_size)
+    pdf_pages.savefig(F, bbox_inches='tight')
+    plt.clf()
+    pdf_pages.close()
+
+if __name__ == "__main__":
+    plot_compression()
--- experiments/eval_scripts/paper_plot_latency_dht.py
+++ experiments/eval_scripts/paper_plot_latency_dht.py
@@ -7,6 +7,7 @@ import sqlite3
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
+
 db_pattern = re.compile("local_dht_benchmark_k(.*)_a(.*)_n(.*)_l(.*).db")
 FETCH_CLIENT_DATA = "SELECT time_store_chunk, time_fetch_addr + time_fetch_nonce + time_fetch_chunk " \
                     "FROM CLIENT_STORE_GET " \
@@ -39,126 +40,128 @@ def extract_address_all_data_from_db(db_path, start_data, end_data):
     res2 = extract_address_data_from_db(FETCH_ADDRESS_GET, db_path, start_data, end_data)
     return res1, res2
 
-latencies = [20]
 
-path = "../data/local_dht_benchmark_k10_a3"
+def plot_latency_dht():
+    latencies = [20]
 
-data_store = []
-data_get = []
-data_get_addr = []
-data_store_addr = []
-for filename in os.listdir(path):
-    if filename.endswith(".db"):
-        matching = db_pattern.match(filename)
-        if matching:
-            kval = int(matching.group(1))
-            aval = int(matching.group(2))
-            num_nodes = int(matching.group(3))
-            latency = int(matching.group(4)) * 2
-            store_data, get_data = extract_client_data_from_db(os.path.join(path, filename), 25, 1025)
-            data_store.append([num_nodes, latency] + store_data.tolist())
-            data_get.append([num_nodes, latency] + get_data.tolist())
+    path = "../data/local_dht_benchmark_k10_a3"
 
-            store_data_addr, get_data_addr = extract_address_all_data_from_db(os.path.join(path, filename), 25, 1025)
-            data_store_addr.append([num_nodes, latency] + store_data_addr.tolist())
-            data_get_addr.append([num_nodes, latency] + get_data_addr.tolist())
+    data_store = []
+    data_get = []
+    data_get_addr = []
+    data_store_addr = []
+    for filename in os.listdir(path):
+        if filename.endswith(".db"):
+            matching = db_pattern.match(filename)
+            if matching:
+                kval = int(matching.group(1))
+                aval = int(matching.group(2))
+                num_nodes = int(matching.group(3))
+                latency = int(matching.group(4)) * 2
+                store_data, get_data = extract_client_data_from_db(os.path.join(path, filename), 25, 1025)
+                data_store.append([num_nodes, latency] + store_data.tolist())
+                data_get.append([num_nodes, latency] + get_data.tolist())
 
-data_store = np.asarray(data_store)
-data_get = np.asarray(data_get)
-data_get_addr = np.asarray(data_get_addr)
-data_store_addr = np.asarray(data_store_addr)
+                store_data_addr, get_data_addr = extract_address_all_data_from_db(os.path.join(path, filename), 25, 1025)
+                data_store_addr.append([num_nodes, latency] + store_data_addr.tolist())
+                data_get_addr.append([num_nodes, latency] + get_data_addr.tolist())
 
-data_store = data_store[data_store[:, 0].argsort()]
-data_get = data_get[data_get[:, 0].argsort()]
-data_get_addr = data_get_addr[data_get_addr[:, 0].argsort()]
-data_store_addr = data_store_addr[data_store_addr[:, 0].argsort()]
+    data_store = np.asarray(data_store)
+    data_get = np.asarray(data_get)
+    data_get_addr = np.asarray(data_get_addr)
+    data_store_addr = np.asarray(data_store_addr)
 
-
-
-print data_store.shape
-print data_get.shape
-print data_get_addr.shape
-print data_store_addr.shape
-
-
-def get_data_for_latency(latency, data):
-    temp = data[data[:, 1] == latency]
-    temp = temp[temp[:, 0].argsort()]
-    return temp[:, 0], temp[:, 2:]
+    data_store = data_store[data_store[:, 0].argsort()]
+    data_get = data_get[data_get[:, 0].argsort()]
+    data_get_addr = data_get_addr[data_get_addr[:, 0].argsort()]
+    data_store_addr = data_store_addr[data_store_addr[:, 0].argsort()]
 
 
 
-#########
-# PLOTS #
-#########
-
-#---------------------------- GLOBAL VARIABLES --------------------------------#
-# figure settings
-fig_width_pt = 300.0                        # Get this from LaTeX using \showthe
-inches_per_pt = 1.0/72.27*2                 # Convert pt to inches
-golden_mean = ((math.sqrt(5)-1.0)/2.0)*.8   # Aesthetic ratio
-fig_width = fig_width_pt*inches_per_pt      # width in inches
-fig_height =(fig_width*golden_mean)           # height in inches
-fig_size = [fig_width,fig_height/1.22]
-
-params = {'backend': 'ps',
-    'axes.labelsize': 18,
-    'legend.fontsize': 16,
-    'xtick.labelsize': 16,
-    'ytick.labelsize': 16,
-    'font.size': 16,
-    'figure.figsize': fig_size,
-    'font.family': 'times new roman'}
+    print data_store.shape
+    print data_get.shape
+    print data_get_addr.shape
+    print data_store_addr.shape
 
 
-pdf_pages = PdfPages('../plots/plot_compression-rate.pdf')
-fig_size = [fig_width, fig_height / 1.8]
-
-plt.rcParams.update(params)
-plt.axes([0.12, 0.32, 0.85, 0.63], frameon=True)
-plt.rc('pdf', fonttype=42)  # IMPORTANT to get rid of Type 3
+    def get_data_for_latency(latency, data):
+        temp = data[data[:, 1] == latency]
+        temp = temp[temp[:, 0].argsort()]
+        return temp[:, 0], temp[:, 2:]
 
 
-def compute_avg_std(data):
-    return np.average(data, axis=1), np.std(data, axis=1)
+
+    #########
+    # PLOTS #
+    #########
+
+    #---------------------------- GLOBAL VARIABLES --------------------------------#
+    # figure settings
+    fig_width_pt = 300.0                        # Get this from LaTeX using \showthe
+    inches_per_pt = 1.0/72.27*2                 # Convert pt to inches
+    golden_mean = ((math.sqrt(5)-1.0)/2.0)*.8   # Aesthetic ratio
+    fig_width = fig_width_pt*inches_per_pt      # width in inches
+    fig_height =(fig_width*golden_mean)           # height in inches
+    fig_size = [fig_width,fig_height/1.22]
+
+    params = {'backend': 'ps',
+        'axes.labelsize': 18,
+        'legend.fontsize': 16,
+        'xtick.labelsize': 16,
+        'ytick.labelsize': 16,
+        'font.size': 16,
+        'figure.figsize': fig_size,
+        'font.family': 'times new roman'}
 
 
-latency = 20
+    pdf_pages = PdfPages('../plots/plot_compression-rate.pdf')
+    fig_size = [fig_width, fig_height / 1.8]
 
-f,  ax1 = plt.subplots()
+    plt.rcParams.update(params)
+    plt.axes([0.12, 0.32, 0.85, 0.63], frameon=True)
+    plt.rc('pdf', fonttype=42)  # IMPORTANT to get rid of Type 3
 
-nodes_s, data_s = get_data_for_latency(latency, data_store)
-mean_s, std_s = compute_avg_std(data_s)
-nodes_g, data_g = get_data_for_latency(latency, data_get)
-mean_g, std_g = compute_avg_std(data_g)
+    def compute_avg_std(data):
+        return np.average(data, axis=1), np.std(data, axis=1)
 
-_, addr_data_s = get_data_for_latency(latency, data_store_addr)
-addr_mean_s, addr_std_s = compute_avg_std(addr_data_s)
-_, addr_data_g = get_data_for_latency(latency, data_get_addr)
-addr_mean_g, addr_std_g = compute_avg_std(addr_data_g)
+    latency = 20
 
-ind = np.arange(1, len(nodes_g.tolist())+1)
-width = 0.35
+    f,  ax1 = plt.subplots()
 
-colours = ['0.1', '0.3', '0.4', '0.75']
+    nodes_s, data_s = get_data_for_latency(latency, data_store)
+    mean_s, std_s = compute_avg_std(data_s)
+    nodes_g, data_g = get_data_for_latency(latency, data_get)
+    mean_g, std_g = compute_avg_std(data_g)
 
-rects1 = ax1.bar(ind, mean_s, width, color=colours[0], yerr=std_g, error_kw=dict(ecolor='0.75', lw=2, capsize=5, capthick=2))
-rects2 = ax1.bar(ind + width, mean_g, width, color=colours[1], yerr=std_s, error_kw=dict(ecolor='0.25', lw=2, capsize=5, capthick=2))
-rects3 = ax1.bar(ind, addr_mean_s, width, color=colours[2], yerr=addr_std_s, error_kw=dict(ecolor='0.75', lw=2, capsize=5, capthick=2))
-rects4 = ax1.bar(ind + width, addr_mean_g, width, color=colours[3], yerr=addr_std_g, error_kw=dict(ecolor='0.25', lw=2, capsize=5, capthick=2))
+    _, addr_data_s = get_data_for_latency(latency, data_store_addr)
+    addr_mean_s, addr_std_s = compute_avg_std(addr_data_s)
+    _, addr_data_g = get_data_for_latency(latency, data_get_addr)
+    addr_mean_g, addr_std_g = compute_avg_std(addr_data_g)
 
+    ind = np.arange(1, len(nodes_g.tolist())+1)
+    width = 0.35
 
-ax1.set_ylabel("Time in milliseconds [ms]")
-ax1.set_xticks(ind + width)
-ax1.set_xticklabels((map(lambda x: str(int(x)), nodes_g.tolist())))
-ax1.set_xlabel("Number of nodes")
+    colours = ['0.1', '0.3', '0.4', '0.75']
 
-ax1.legend((rects1[0], rects2[0], rects3[0], rects4[0]), ('Put', 'Get', 'Routing Put', 'Routing Get'), loc="upper left", ncol=2)
+    rects1 = ax1.bar(ind, mean_s, width, color=colours[0], yerr=std_g, error_kw=dict(ecolor='0.75', lw=2, capsize=5, capthick=2))
+    rects2 = ax1.bar(ind + width, mean_g, width, color=colours[1], yerr=std_s, error_kw=dict(ecolor='0.25', lw=2, capsize=5, capthick=2))
+    rects3 = ax1.bar(ind, addr_mean_s, width, color=colours[2], yerr=addr_std_s, error_kw=dict(ecolor='0.75', lw=2, capsize=5, capthick=2))
+    rects4 = ax1.bar(ind + width, addr_mean_g, width, color=colours[3], yerr=addr_std_g, error_kw=dict(ecolor='0.25', lw=2, capsize=5, capthick=2))
 
-f.suptitle("RTT-%d average latency DHT operations" % latency, fontsize=24, y=1.02)
+    ax1.set_ylabel("Time in milliseconds [ms]")
+    ax1.set_xticks(ind + width)
+    ax1.set_xticklabels((map(lambda x: str(int(x)), nodes_g.tolist())))
+    ax1.set_xlabel("Number of nodes")
 
-F = plt.gcf()
-F.set_size_inches(fig_size)
-pdf_pages.savefig(F, bbox_inches='tight')
-plt.clf()
-pdf_pages.close()
+    ax1.legend((rects1[0], rects2[0], rects3[0], rects4[0]), ('Put', 'Get', 'Routing Put', 'Routing Get'), loc="upper left", ncol=2)
+
+    f.suptitle("RTT-%d average latency DHT operations" % latency, fontsize=24, y=1.02)
+
+    F = plt.gcf()
+    F.set_size_inches(fig_size)
+    pdf_pages.savefig(F, bbox_inches='tight')
+    plt.clf()
+    pdf_pages.close()
+
+if __name__ == "__main__":
+    plot_latency_dht()
